829fa0460547bc592250c867c15de1d643eb7c2e
Justyn Mencl <92409211+JM3N5LICE@users.noreply.github.com>
2024-03-21 20:50:53 -0600

    fixed our sadness

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.CLIENT_SECRET,
-  // baseURL: 'https://webservicesfinal.onrender.com',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://webservicesfinal.onrender.com',
+  // baseURL: 'http://localhost:8080',
   clientID: process.env.CLIENT_ID,
-  // issuerBaseURL: process.env.ISSUER_BASE_URL
-  issuerBaseURL: 'https://dev-gm1z4qm5wulqlarf.us.auth0.com'
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
