a8511c722bcceb34105881902ecd8910f856dc52
Otavio Silva <otaviovalerianoss@gmail.com>
2024-04-02 20:49:15 -0600

    testing, trying

--- _tests_/get.spec.js
+++ _tests_/get.spec.js
@@ -1,16 +1,16 @@
-const app = require('../server')
+const server = require('../server')
 const supertest = require('supertest');
 const { expect } = require('@jest/globals');
-const request = supertest(app)
+const request = supertest(server)
 
 
 
 describe('Test Handlers', () => {
-    test('responds to /', async () => {
-        const res = await request.get('/books');
-        // expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(res.statusCode).toBe(200)
-    })
+    test('responds to /books', async () => {
+        const res = await request(server).get('/books');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(typeof res.statusCode).toBe('number')
+    });
 
     // test('responds to /users', async () => {
     //     const res = await request.get('/books');
