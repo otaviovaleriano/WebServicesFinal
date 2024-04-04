fc47582482aeb3db79fc382222f3fd6b9a66f3e0
Otavio Silva <otaviovalerianoss@gmail.com>
2024-04-04 00:52:04 -0600

    reversing cors stuff

--- server.js
+++ server.js
@@ -33,21 +33,13 @@ app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
-// CORS middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
 app
   .use(bodyParser.json())
-  // .use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   next();
-  // })
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
   .use('/', require('./routes'));
 
 
