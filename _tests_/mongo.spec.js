4ce959ce20cdd378a2e505adccf54b5a5ae00fac
Otavio Silva <otaviovalerianoss@gmail.com>
2024-04-04 01:05:10 -0600

    mongo tests working

--- _tests_/mongo.spec.js
+++ _tests_/mongo.spec.js
@@ -1,46 +1,46 @@
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
+const { MongoClient } = require("mongodb");
+const dotenv = require("dotenv");
 dotenv.config();
 
-describe('insert', () => {
-    let connection;
-    let db;
+describe("insert", () => {
+  let connection;
+  let db;
 
-    beforeAll(async () => {
-
-        connection = await MongoClient.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        db = await connection.db('books')
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    afterAll(async() => {
-        await connection.close()
-    })
+    db = await connection.db("books");
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    it('should insert a new user into the users collection', async () => {
-        const users = db.collection('books');
+  it("should insert a new book into the book collection", async () => {
+    const book = db.collection("books");
 
-        const mockUser = {
-            id: 'some-user-id',
-            firstName: "Emily",
-            lastName: "Button",
-            email: "emilyButton@gmail.com",
-            age: 25,
-        }
+    const mockBook = {
+      title: "Book Test",
+      author_id: "65f3a916dde35f7c857e7ba3",
+      genre: "Fiction",
+      published_year: 1960,
+      publisher: "J. B. Lippincott & Co.",
+      pages: 340,
+      isbn: "9780061120082",
+    };
 
-        await users.insertOne(mockUser)
+    await book.insertOne(mockBook);
 
-        const insertedUser = await users.findOne({ id: 'some-user-id' });
+    const insertedBook = await book.findOne({
+      author_id: "65f3a916dde35f7c857e7ba3",
+    });
 
-        expect(insertedUser).toEqual(mockUser)
-    },
-        
-    it('should delete a user from the users collection', async () => {
-        const users = db.collection('users')
-        await users.deleteMany({ id: 'some-user-id' })
-        const deletedUser = await users.findOne({ id: 'some-user-id' });
-        expect(deletedUser).toEqual(null)
-    })
-)
-})
+    console.log("Inserted Book:", insertedBook);
+    console.log("Mock Book:", mockBook);
+    delete insertedBook._id;
+    delete mockBook._id;
+    expect(insertedBook).toEqual(mockBook);
+  });
+
+});
